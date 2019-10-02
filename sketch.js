0afd916464a65ffa17dfc92df5af48f75c565bdb
emanueleghebaur <37125221+emanueleghebaur@users.noreply.github.com>
2019-10-02 22:38:52 +0200

    Cambio

--- sketch.js
+++ sketch.js
@@ -1,54 +1,26 @@
-
-// Position Variables
-let x = 0;
-let y = 0;
-
-// Speed - Velocity
-let vx = 0;
-let vy = 0;
-
-// Acceleration
-let ax = 0;
-let ay = 0;
-
-let vMultiplier = 0.007;
-let bMultiplier = 0.6;
+function preload() {
+    // put preload code here
+    Adieu = loadFont('assets/Adieu-Bold.otf');
+}
 
 function setup() {
-  createCanvas(displayWidth, displayHeight);
-  fill(0);
+    createCanvas(windowWidth, windowHeight);
+    background(10);
+    // put setup code here
 }
 
 function draw() {
-  background(255);
-  ballMove();
-  ellipse(x, y, 30, 30);
+    strokeWeight(0.5);
+    textSize(33);
+    textFont(Adieu);
+    textAlign(CENTER, CENTER);
+    fill(10);
+    var t = 'KWESTURAH  AWARDS  2019  SOON.';
+    text(t, width / 4, height * 2 / 5, width / 2);
 }
 
-function ballMove() {
-  ax = accelerationX;
-  ay = accelerationY;
-
-  vx = vx + ay;
-  vy = vy + ax;
-  y = y + vy * vMultiplier;
-  x = x + vx * vMultiplier;
-
-  // Bounce when touch the edge of the canvas
-  if (x < 0) {
-    x = 0;
-    vx = -vx * bMultiplier;
-  }
-  if (y < 0) {
-    y = 0;
-    vy = -vy * bMultiplier;
-  }
-  if (x > width - 20) {
-    x = width - 20;
-    vx = -vx * bMultiplier;
-  }
-  if (y > height - 20) {
-    y = height - 20;
-    vy = -vy * bMultiplier;
-  }
+function mouseDragged() {
+	noStroke();
+    fill('240');
+    ellipse(mouseX, mouseY, 100);
 }
