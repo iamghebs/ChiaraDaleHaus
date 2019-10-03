2d4374f7800bf097cf110be59555928520a7f76b
emanueleghebaur <37125221+emanueleghebaur@users.noreply.github.com>
2019-10-03 02:48:06 +0200

    5 dita

--- sketch.js
+++ sketch.js
@@ -6,41 +6,49 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(10);
-	frameRate(50);
-	//noStroke();
-	push();
-	stroke(255);
-	strokeWeight(1);
-	noFill();
-	textSize(22);
-	text('gratta lo schermo e scopri il messaggio', width / 4, height /15, width / 2);
-	pop();
+    frameRate(50);
+    //noStroke();
+    push();
+    stroke(255);
+    strokeWeight(1);
+    noFill();
+    textSize(22);
+    text('PC: gratta lo schermo e scopri il messaggio', width / 4, height / 15, width / 2);
+    text('touch: tocca lo schermo con più di 5 dita', width / 4, height * 2 / 15, width / 2);
+    pop();
     // put setup code here
 }
 
 function draw() {
-
-	push();
-	fill(10);
-	textSize(50);
+    if (touches.length > 5)
+        var colore = 'rgb(28, 230, 96)';
+    else var colore = 10;
+    fill(colore);
+    textSize(50);
     textFont(Adieu);
     textAlign(CENTER, CENTER);
     var t = 'KWESTURAH  AWARDS  2019  SOON.';
     text(t, width / 4, height * 2 / 5, width / 2);
-	pop();
-	push();
-	fill('rgb(28, 230, 96)');
-	ellipse(mouseX, mouseY, 100);
-	pop();
+
+    // fill('rgb(28, 230, 96)');
+    // let speed = abs(winMouseX - pwinMouseX);
+    // ellipse(mouseX, mouseY, 20 + speed);
+    // pop();
 }
 
-// function mouseDragged() {
-// 	noStroke();
-//     fill('rgb(28, 230, 96)');
-//     ellipse(mouseX, mouseY, 100);
-// 	return false;
-// }
+function mouseDragged() {
+    push();
+    noStroke();
+    fill('rgb(28, 230, 96)');
+    ellipse(mouseX, mouseY, 100);
+    return false;
+    pop();
+}
 
-function mouseClicked(){
-	console.log("Press");
+function touchStarted() {
+    return false;
+}
+
+function mouseClicked() {
+    console.log("Press");
 }
